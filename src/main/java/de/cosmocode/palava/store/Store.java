16ae8268a8c1dcffeba8acef4058a1a682ff5d1e
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-03-23 19:10:26 +0000

    added new bridge independent Store interface

---
+++ src/main/java/de/cosmocode/palava/store/Store.java
@@ -0,0 +1,67 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007-2010  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package de.cosmocode.palava.store;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A {@link Store} allows storing binary data.
+ * Stored data can be accessed using a string identifier.
+ *
+ * @author Willi Schoenborn
+ */
+public interface Store {
+
+    /**
+     * Stores the given stream in this store.
+     * 
+     * @param stream the binary data being stored
+     * @return a generated identifier, which can be used to retrieve or delete the stored data
+     * @throws NullPointerException if stream is null
+     * @throws IOException if saving failed
+     */
+    String create(InputStream stream) throws IOException;
+    
+    /**
+     * Retrieves the binary data for a given identifier.
+     * 
+     * @param identifier the identifier of the binary data being retrieved
+     * @return the InputStream assicuated with the given identifier
+     * @throws NullPointerException if identifier is null
+     * @throws IllegalArgumentException if identifier is not valid, according to restrictions
+     *         introduced by sub classes
+     * @throws IOException if reading failed
+     */
+    InputStream read(String identifier) throws IOException;
+    
+    /**
+     * Removes binary data from this store.
+     * 
+     * @param identifier the identifier of the data being deleted.
+     * @throws NullPointerException if identifier is null
+     * @throws IllegalArgumentException if identifier is not valid, according to restrictions
+     *         introduced by sub classes
+     * @throws IOException if deletion failed, caused by unknown identifier or general io issues
+     */
+    void delete(String identifier) throws IOException;
+    
+}
---
+++ src/test/java/de/cosmocode/palava/store/AbstractStoreTest.java
@@ -0,0 +1,137 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007-2010  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package de.cosmocode.palava.store;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.cosmocode.junit.UnitProvider;
+
+/**
+ * Abstract test for {@link Store} implementations.
+ *
+ * @author Willi Schoenborn
+ */
+public abstract class AbstractStoreTest implements UnitProvider<Store> {
+
+    private static final String UTF_8 = "UTF-8";
+    
+    /**
+     * Tests {@link Store#create(InputStream)} with a null stream.
+     * 
+     * @throws IOException should not happen 
+     */
+    @Test(expected = NullPointerException.class)
+    public void createNull() throws IOException {
+        unit().create(null);
+    }
+    
+    /**
+     * Tests {@link Store#create(InputStream)} with a {@link ByteArrayInputStream}.
+     * 
+     * @throws IOException should not happen
+     */
+    @Test
+    public void create() throws IOException {
+        final InputStream stream = new ByteArrayInputStream("data".getBytes());
+        final String identifier = unit().create(stream);
+        Assert.assertNotNull(identifier);
+        Assert.assertFalse(identifier.isEmpty());
+    }
+
+    /**
+     * Tests {@link Store#read(String)} with a null identifier.
+     * 
+     * @throws IOException should not happen
+     */
+    @Test(expected = NullPointerException.class)
+    public void readNull() throws IOException {
+        unit().read(null);
+    }
+    
+    /**
+     * Tests {@link Store#read(String)} with a {@link ByteArrayInputStream}.
+     * 
+     * @throws IOException should not happen
+     */
+    @Test
+    public void read() throws IOException {
+        final Store unit = unit();
+        final InputStream stream = new ByteArrayInputStream("data".getBytes(UTF_8));
+        final String identifier = unit.create(stream);
+        final InputStream read = unit.read(identifier);
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        IOUtils.copy(read, out);
+        Assert.assertEquals("data", out.toString(UTF_8));
+    }
+    
+    /**
+     * Tests {@link Store#read(String)} using an identifier which does
+     * not point to an existing entry.
+     * 
+     * @throws IOException expected
+     */
+    @Test(expected = IOException.class)
+    public void readMissing() throws IOException {
+        unit().read("no-such-key");
+    }
+    
+    /**
+     * Tests {@link Store#delete(String)} with a null identifier.
+     * 
+     * @throws IOException should not happen
+     */
+    @Test(expected = NullPointerException.class)
+    public void deleteNull() throws IOException {
+        unit().delete(null);
+    }
+
+    /**
+     * Tests {@link Store#delete(String)} with a {@link ByteArrayInputStream}.
+     * 
+     * @throws IOException should not happen
+     */
+    @Test
+    public void delete() throws IOException {
+        final Store unit = unit();
+        final InputStream stream = new ByteArrayInputStream("data".getBytes(UTF_8));
+        final String identifier = unit.create(stream);
+        unit.delete(identifier);
+    }
+    
+    /**
+     * Tests {@link Store#delete(String)} using an identifier which does
+     * not point to an existing entry.
+     * 
+     * @throws IOException expected
+     */
+    @Test(expected = IOException.class)
+    public void deleteMissing() throws IOException {
+        unit().delete("no-such-key");
+    }
+    
+}
